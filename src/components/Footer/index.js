9cff8c71835a267a4c844bf546b7aff682ba6390
shashi Bhushan <bhushans727@gmail.com>
2022-11-05 12:56:56 +0530

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='foots'>
+            © 2022 Shashi Bhushan Kumar
+        </div>
+      )
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,221 @@
+import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+    const [dataBolly,setDataBolly] = useState([]);
+    const [dataFit,setDataFit] = useState([])
+    const [dataTech,setDataTech] = useState([])
+    const [dataFood,setDataFood] = useState([])
+    const [dataTop,setDataTop] = useState([]);
+
+    useEffect(() => {
+
+
+        fetch("https://blog-api-node-bhushan.herokuapp.com/api/bollywood")
+            .then(response => response.json())
+            .then((details) => {
+                setDataBolly(details)
+            })
+
+        fetch("https://blog-api-node-bhushan.herokuapp.com/api/fitness")
+            .then(response => response.json())
+            .then((details) => {
+                setDataFit(details)
+            })
+
+        fetch("https://blog-api-node-bhushan.herokuapp.com/api/technology")
+            .then(response => response.json())
+            .then((details) => {
+                setDataTech(details)
+            })
+
+        fetch("https://blog-api-node-bhushan.herokuapp.com/api/food")
+            .then(response => response.json())
+            .then((details) => {
+                setDataFood(details)
+            })
+
+        fetch("https://blog-api-node-bhushan.herokuapp.com/api/top")
+            .then(response => response.json())
+            .then((details) => {
+                setDataTop(details)
+            })
+
+
+    },[])
+
+
+  return (
+    <div className='home'>
+
+        {/* The Banner Part */}
+        <div className='bannerContainer'>
+            <div className="leftBanner">
+                <h1 className='bannerOneTitle'>Title of Vertical Gallery</h1>
+            </div>
+            <div className="rightBanner">
+                <div className="rightSubBanner" id='firstSBanner'>
+
+                </div>
+                <div className="rightSubBanner" id='secondSBanner'>
+                    
+                </div>
+            </div>
+        </div>
+
+
+        {/* latest news three cards */}
+        <h1 className="headLine">
+          Latest Bollywood Article
+        </h1>
+
+        <div className='cardContainer'>
+        {
+            dataBolly.slice(0,3).map((desc,index) => {
+                return(
+                        <Link className='blogItem-link' to={`/bollywood/${desc.id}`} key={index}>
+                    <div className='card' >
+                        <img src={desc.image} alt="not found" className='cardImg' />
+                        <div className="cardTitle">
+                            <h3 className="title">
+                                    {desc.title}
+                            </h3>
+                        </div>
+                        <h5 className="dateTitle">
+                        release date : <span> {desc.date}</span>
+                        </h5>
+                    </div>
+                </Link> 
+                )
+            })
+        }
+        </div>
+           
+        <h1 className="headLine">
+          Latest Fitness Stories
+        </h1>
+
+        <div className='cardContainer'>
+        {
+            dataFit.slice(0,3).map((desc,index) => {
+                return(
+                        <Link className='blogItem-link' to={`/fitness/${desc.id}`} key={index}>
+                    <div className='card' >
+                        <img src={desc.image} alt="not found" className='cardImg' />
+                        <div className="cardTitle">
+                            <h3 className="title">
+                                    {desc.title}
+                            </h3>
+                        </div>
+                        <h5 className="dateTitle">
+                        release date : <span> {desc.date}</span>
+                        </h5>
+                    </div>
+                </Link> 
+                )
+            })
+        }
+        </div>
+
+
+        <div className='mainContainer'>
+        
+        <div className="leftContainer">
+        <h1 className="headingArt">
+          Latest Top Article
+        </h1>
+
+        {
+            dataTop.slice(0,4).map((info,index) => {
+                return(
+            <Link className='blogItem-link' to={`/top/${info.id}`} key={index}>
+            <div className="cardArticle">
+                    <img src={info.image} alt="link not found" />
+                    <div className="discr">
+                        <div className="titleDisc">
+                            {info.title}
+                        </div>
+                        <h4 className="categ">Category : <span>Hollywood</span></h4>
+                        <h4 className="issueD">issue Date : <span>{info.date}</span></h4>
+                    </div>
+            </div>
+        </Link>
+
+                )
+            })
+        }
+        
+
+        </div>
+        <div className="ritContainer">
+
+            <div className="advertise">
+                Advertisments
+            </div>
+            <div className="advertise">
+                Advertisments
+            </div>
+        </div>    
+    </div>
+        
+        {/* Technology */}
+        <h1 className="headLine">
+          Latest Technology News
+        </h1>
+        
+        <div className='cardContainer'>
+        {
+            dataTech.slice(0,3).map((desc,index) => {
+                return(
+                        <Link className='blogItem-link' to={`/technology/${desc.id}`} key={index}>
+                    <div className='card' >
+                        <img src={desc.image} alt="not found" className='cardImg' />
+                        <div className="cardTitle">
+                            <h3 className="title">
+                                    {desc.title}
+                            </h3>
+                        </div>
+                        <h5 className="dateTitle">
+                        release date : <span> {desc.date}</span>
+                        </h5>
+                    </div>
+                </Link> 
+                )
+            })
+        }
+        </div>
+
+        {/* Food Section */}
+        <h1 className="headLine">
+          Latest Food Article
+        </h1>
+        <div className='cardContainer'>
+        {
+            dataFood.slice(0,3).map((desc,index) => {
+                return(
+                        <Link className='blogItem-link' to={`/food/${desc.id}`} key={index}>
+                    <div className='card' >
+                        <img src={desc.image} alt="not found" className='cardImg' />
+                        <div className="cardTitle">
+                            <h3 className="title">
+                                    {desc.title}
+                            </h3>
+                        </div>
+                        <h5 className="dateTitle">
+                        release date : <span> {desc.date}</span>
+                        </h5>
+                    </div>
+                </Link> 
+                )
+            })
+        }
+        </div>
+
+    </div>
+  )
+}
+
+export default Home;
